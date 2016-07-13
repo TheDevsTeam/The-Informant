76586a86c0adb2fb6be3148f2f05bfb6f64880b6
Cheikh Ndiongue <Cheikhndiongue3@gmail.com>
2016-07-13 18:19:29 -0400

    added crud functionality into app.

--- app/controllers/infos_controller.rb
+++ app/controllers/infos_controller.rb
@@ -1,5 +1,8 @@
 class InfosController < ApplicationController
-  def index
+  before_action :find_info, only: [:show, :edit, :update, :destroy]
+
+  def indexd
+    @infos = Info.all.order("created_at DESC")
   end
 
   def new
@@ -8,12 +11,40 @@ class InfosController < ApplicationController
 
   def create
     @info = Info.new(info_params)
+
+    if @info.save
+      redirect_to @info, notice: "You have successfully created the new info"
+    else
+      render 'new'
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @info.update(info_params)
+      redirect_to @info, notice: "Info was successfully updated"
+    else
+      render 'edit'
+  end
+
+  def destroy\
+    @info.destroy
+    redirect_to root_path
   end
 
   private
 # this is so info is only permitted to add title and description
   def pin_params
     params.require(:info).permit(:title, :description)
+  end
+
+  def find_info
+    @info = Info.find(params[:id])
   end
 
 
